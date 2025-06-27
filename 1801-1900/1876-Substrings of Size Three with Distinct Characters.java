3e8136b50113f5e629f47b922dd943446942f52e
Nithin Manikkan <mnthinsavi@gmail.com>
2025-06-27 15:32:32 +0530

    .

--- 1801-1900/1876-Substrings of Size Three with Distinct Characters.java
+++ 1801-1900/1876-Substrings of Size Three with Distinct Characters.java
@@ -21,7 +21,7 @@
 // The only good substring of length 3 is "xyz".
 // Example 2:
 
-// Input: s = "aababcabc"
+// Input: s = "aababcabc" 
 // Output: 4
 // Explanation: There are 7 substrings of size 3: "aab", "aba", "bab", "abc", "bca", "cab", and "abc". 
 // The good substrings are "abc", "bca", "cab", and "abc".
