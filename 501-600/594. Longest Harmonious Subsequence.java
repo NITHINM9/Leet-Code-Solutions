641649de08af07ed5fa72e624a31949b057a7038
MNithin <mnithinsavi@gmail.com>
2025-06-30 12:27:38 +0530

    Solution 2

--- 501-600/594. Longest Harmonious Subsequence.java
+++ 501-600/594. Longest Harmonious Subsequence.java
@@ -70,3 +70,18 @@ class Solution {
 
 
 // solution 2 :-
+class Solution {
+    public int findLHS(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int result = 0;
+        for(int i : nums){
+            map.put(i , map.getOrDefault( i , 0) + 1);
+        }
+        for (int key : map.keySet()) {
+            if (map.containsKey(key + 1)) {
+                result = Math.max(result, map.get(key) + map.get(key + 1));
+            }
+        }
+        return result;
+    }
+}
