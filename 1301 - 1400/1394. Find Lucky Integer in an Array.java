e195be5c50cc25ad6360696c864c0b69a23b0753
M Nithin <mnithinsavi@gmail.com>
2025-07-05 23:52:55 +0530

    1394. Find Lucky Integer in an Array solution 2

--- 1301 - 1400/1394. Find Lucky Integer in an Array.java
+++ 1301 - 1400/1394. Find Lucky Integer in an Array.java
@@ -46,3 +46,19 @@ class Solution {
     }
 }
 
+//solution 2 ->
+
+class Solution {
+    public int findLucky(int[] arr) {
+        int[] freq = new int[501];
+        for (int num : arr) {
+            freq[num]++;
+        }
+        for (int i = 500; i >= 1; i--) {
+            if (freq[i] == i) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
