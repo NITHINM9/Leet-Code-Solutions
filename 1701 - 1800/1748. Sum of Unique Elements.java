e3d45a3220faec41794e5d59281524bca487fa75
M Nithin <mnithinsavi@gmail.com>
2025-07-06 22:37:47 +0530

    1748. Sum of Unique Elements

--- 1701 - 1800/1748. Sum of Unique Elements.java
+++ 1701 - 1800/1748. Sum of Unique Elements.java
@@ -45,3 +45,20 @@ class Solution {
         return sum;
     }
 }
+
+//solution 2->
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] freq = new int[101]; 
+        for (int num : nums) {
+            freq[num]++;
+        }
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (freq[i] == 1) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
