e68d7b98d72f0571b29b9721249a190b46d126e4
M Nithin <mnithinsavi@gmail.com>
2025-07-15 21:05:39 +0530

    3136. Valid Word solution

--- 3101 -3200/3136. Valid Word.java
+++ 3101 -3200/3136. Valid Word.java
@@ -53,3 +53,26 @@
 // 1 <= word.length <= 20
 // word consists of English uppercase and lowercase letters, digits, '@', '#', and '$'.
 
+//solution
+
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3){
+            return false;
+        }
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+         for (char c : word.toCharArray()) {
+            if (!Character.isLetterOrDigit(c)) return false;
+            if (Character.isLetter(c)) {
+                char lower = Character.toLowerCase(c);
+                if (lower == 'a' || lower == 'e' || lower == 'i' || lower == 'o' || lower == 'u') {
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+}
