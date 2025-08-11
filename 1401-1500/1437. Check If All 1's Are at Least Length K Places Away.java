7ba25f0037388e7713d4735d8f021a57a69340c6
MNithin <mnithinsavi@gmail.com>
2025-08-11 17:55:05 +0530

    Check If All 1's Are at Least Length K Places Away solution

--- 1401-1500/1437. Check If All 1's Are at Least Length K Places Away.java
+++ 1401-1500/1437. Check If All 1's Are at Least Length K Places Away.java
@@ -1,24 +1,48 @@
-1437. Check If All 1's Are at Least Length K Places Away
+// 1437. Check If All 1's Are at Least Length K Places Away
 
-Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
+// Given an binary array nums and an integer k, return true if all 1's are at least k places away from each other, otherwise return false.
 
-Example 1:
+// Example 1:
 
-Input: nums = [1,0,0,0,1,0,0,1], k = 2
-Output: true
-Explanation: Each of the 1s are at least 2 places away from each other.
-Example 2:
+// Input: nums = [1,0,0,0,1,0,0,1], k = 2
+// Output: true
+// Explanation: Each of the 1s are at least 2 places away from each other.
+// Example 2:
 
 
-Input: nums = [1,0,0,1,0,1], k = 2
-Output: false
-Explanation: The second 1 and third 1 are only one apart from each other.
+// Input: nums = [1,0,0,1,0,1], k = 2
+// Output: false
+// Explanation: The second 1 and third 1 are only one apart from each other.
  
 
-Constraints:
+// Constraints:
 
-1 <= nums.length <= 105
-0 <= k <= nums.length
-nums[i] is 0 or 1
+// 1 <= nums.length <= 105
+// 0 <= k <= nums.length
+// nums[i] is 0 or 1
 
 
+//solution
+class Solution {
+    public boolean kLengthApart(int[] nums, int k) {
+        int j = 0 ,diff = 0, i = 0;
+        int last = 0;
+        while(i < nums.length){
+            if(nums[i] == 1){
+                last++;
+                if(last > 1){
+                    diff = i - j - 1;
+                    j = i;
+                    if(diff < k){
+                        return false;
+                    }
+                }
+                else{
+                    j = i;
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+}
