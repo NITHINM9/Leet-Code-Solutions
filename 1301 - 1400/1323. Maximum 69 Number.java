dec5d5f859dee0da6291fb2b47b0cfdca06e2910
mnith <mnithinsavi@gmail.com>
2025-08-16 19:51:19 +0530

    Maximum 69 Number solution

--- 1301 - 1400/1323. Maximum 69 Number.java
+++ 1301 - 1400/1323. Maximum 69 Number.java
@@ -34,3 +34,15 @@
 // num consists of only 6 and 9 digits.
 
 //solution - >
+class Solution {
+    public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+        for(int i = 0; i < digits.length; i++){
+            if(digits[i] == '6'){
+                digits[i] = '9';
+                break;
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
