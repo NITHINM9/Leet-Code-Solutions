632bfab5b3ac38b84b4b08ed65f9c38c1580536f
MNithin <mnithinsavi@gmail.com>
2025-08-18 17:59:14 +0530

    1512. Number of Good Pairs solution 1

--- 1501-1600/1512. Number of Good Pairs.java
+++ 1501-1600/1512. Number of Good Pairs.java
@@ -26,3 +26,19 @@
 
 // 1 <= nums.length <= 100
 // 1 <= nums[i] <= 100
+
+//solution - Brute force
+// Time Complexity: O(n^2)>
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[i] == nums[j] && i < j){
+                    count++;
+                }
+            }
+        }
+        return count++;
+    }
+}
