b3a79103cbb52c6b39e9bd6eb555ad60cad84ae3
M Nithin <mnithinsavi@gmail.com>
2025-08-23 21:05:35 +0530

    412. Fizz Buzz solution

--- 301-400/412. Fizz Buzz.java
+++ 301-400/412. Fizz Buzz.java
@@ -26,3 +26,27 @@
 
 // 1 <= n <= 104
 
+//solution
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % 3 == 0){
+                if(i % 5 == 0){
+                    list.add("FizzBuzz");
+                }
+                else {
+                    list.add("Fizz");
+                }
+            }
+            else if(i % 5 == 0){
+                list.add("Buzz");
+            }
+            else{
+                list.add(Integer.toString(i));
+            }
+        }
+        return list;
+    }
+}
+
