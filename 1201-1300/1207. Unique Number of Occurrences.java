7d229810651212b7db2c86399b38247322cdcdea
MNithin <mnithinsavi@gmail.com>
2025-08-25 21:25:36 +0530

    1207. Unique Number of Occurrences solution

--- 1201-1300/1207. Unique Number of Occurrences.java
+++ 1201-1300/1207. Unique Number of Occurrences.java
@@ -26,3 +26,19 @@
 
 //solution 1 -> Using HashMap and HashSet
 // Time Complexity: O(n)
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int freq : map.values()){
+            if(set.contains(freq)){
+                return false;
+            }
+            set.add(freq);
+        }
+        return true;
+    }
+}
