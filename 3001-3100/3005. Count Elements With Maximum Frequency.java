1868ce1799c1004d3578d8350f8b6b2d6b961566
M Nithin <mnithinsavi@gmail.com>
2025-09-22 21:42:31 +0530

    3005. Count Elements With Maximum Frequency

--- 3001-3100/3005. Count Elements With Maximum Frequency.java
+++ 3001-3100/3005. Count Elements With Maximum Frequency.java
@@ -1,28 +1,49 @@
-3005. Count Elements With Maximum Frequency
+// 3005. Count Elements With Maximum Frequency
 
-You are given an array nums consisting of positive integers.
+// You are given an array nums consisting of positive integers.
 
-Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
+// Return the total frequencies of elements in nums such that those elements all have the maximum frequency.
 
-The frequency of an element is the number of occurrences of that element in the array.
+// The frequency of an element is the number of occurrences of that element in the array.
 
  
 
-Example 1:
+// Example 1:
 
-Input: nums = [1,2,2,3,1,4]
-Output: 4
-Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
-So the number of elements in the array with maximum frequency is 4.
-Example 2:
+// Input: nums = [1,2,2,3,1,4]
+// Output: 4
+// Explanation: The elements 1 and 2 have a frequency of 2 which is the maximum frequency in the array.
+// So the number of elements in the array with maximum frequency is 4.
+// Example 2:
 
-Input: nums = [1,2,3,4,5]
-Output: 5
-Explanation: All elements of the array have a frequency of 1 which is the maximum.
-So the number of elements in the array with maximum frequency is 5.
+// Input: nums = [1,2,3,4,5]
+// Output: 5
+// Explanation: All elements of the array have a frequency of 1 which is the maximum.
+// So the number of elements in the array with maximum frequency is 5.
  
 
-Constraints:
+// Constraints:
 
-1 <= nums.length <= 100
-1 <= nums[i] <= 100
+// 1 <= nums.length <= 100
+// 1 <= nums[i] <= 100
+
+//solution ->
+class Solution {
+    public int maxFrequencyElements(int[] nums) {
+        Map<Integer , Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i] , 0) + 1);
+        }
+        int count = 0, max = 0;
+        for (Integer value : map.values()) {
+            if(value > max){
+                max = value;
+                count = 0;
+            }
+            if(max == value){
+                count+= max;
+            }
+        }
+        return count;
+    }
+}
