dd2f43a494f21a9c446e483013a77230f34a8a1d
M Nithin <mnithinsavi@gmail.com>
2025-10-19 19:50:37 +0530

    Linked List Cycle solution

--- 101-200/141. Linked List Cycle.java
+++ 101-200/141. Linked List Cycle.java
@@ -36,3 +36,30 @@
  
 
 // Follow up: Can you solve it using O(1) (i.e. constant) memory?
+
+
+//solution ->
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head , fast = head;
+        while(fast != null  && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
